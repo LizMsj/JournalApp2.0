fad6dfb7434562e428e36940b1e6c252a06c70c2
kpc <kostroma.kpc@gmail.com>
2023-03-28 15:40:04 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JournalApp2._0.ViewModels.Base;
+
+namespace JournalApp2._0.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string title = "(Без) УП не вырулишь";
+        
+        public string Title 
+        {
+            get { return title; }
+            set { Set(ref title, value); }
+        }
+
+
+    }
+}
